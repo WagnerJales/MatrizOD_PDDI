91378e80297a6f488066f0c19bb846043cadfcfb
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2025-05-08 11:43:56 -0300

    Update app_mapa_od.py

--- app_mapa_od.py
+++ app_mapa_od.py
@@ -110,7 +110,7 @@ with col4:
 col5, col6 = st.columns(2)
 
 with col5:
-    st.subheader("Matriz OD (Gráfico Térmico)")
+    st.subheader("Matriz OD")
     matriz = df_filtrado.groupby(["ORIGEM 2", "DESTINO 2"]).size().unstack(fill_value=0)
     st.plotly_chart(px.imshow(matriz, text_auto=True, color_continuous_scale="Purples", title="Matriz OD"), use_container_width=True)
 
