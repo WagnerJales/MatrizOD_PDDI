d5c36d119632541690e5f308ae9ed2211e6a3be7
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2025-07-30 01:24:16 -0300

    Update app_mapa_od.py

--- app_mapa_od.py
+++ app_mapa_od.py
@@ -173,8 +173,9 @@ st_folium(mapa, width=1600, height=700)
 st.subheader("Matriz OD (Gráfico Térmico)")
 matriz = df_filtrado.groupby(["ORIGEM", "DESTINO"]).size().unstack(fill_value=0)
 altura = 50 * len(matriz)
-fig = px.imshow(matriz, text_auto=True, color_continuous_scale="Purples", height=altura, color_continuous_scale_bounds="minmax")
+fig = px.imshow(matriz, text_auto=True, color_continuous_scale="Purples", height=altura)
 st.plotly_chart(fig, use_container_width=True)
+st.markdown("<div style='margin-bottom: -40px'></div>", unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 with col1:
