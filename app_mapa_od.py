869fd366e05ff6b51ee7bb4c65cb38e040be9194
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2025-08-08 10:46:09 -0300

    Update app_mapa_od.py

--- app_mapa_od.py
+++ app_mapa_od.py
@@ -118,7 +118,11 @@ if modal:
 
 # Mostrar quantidade de registros após filtragem
 st.sidebar.markdown("---")
-st.sidebar.markdown(f"**Total de pesquisas:** `{len(df_filtrado):,}`")
+st.sidebar.markdown("**Total de pesquisas filtradas:**", unsafe_allow_html=True)
+st.sidebar.markdown(
+    f"<span style='font-size: 32px; font-weight: bold; color: #4B8BBE;'>{len(df_filtrado):,}</span>",
+    unsafe_allow_html=True
+
 
 # === Eliminar auto-deslocamentos ===
 df_od = df_filtrado[df_filtrado["ORIGEM"] != df_filtrado["DESTINO"]].copy()
